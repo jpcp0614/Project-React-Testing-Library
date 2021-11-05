75b81046c643e96b4345d58bff4f40941750e51f
jpcp0614 <jpcp0614@gmail.com>
2021-11-05 20:34:47 -0300

    Requisito 4 - OK - altera name

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -5,12 +5,12 @@ import NotFound from '../components/NotFound';
 describe('Requisito 4. Testa o componente NotFound.js', () => {
   beforeEach(() => render(<NotFound />));
   test('Verifica se página contém um h2 com o texto Page requested not found 😭', () => {
-    const headingEl = screen.getByRole('heading', { name: /Page requested not found/i });
+    const headingEl = screen.getByRole('heading', { name: /page requested not found/i });
     expect(headingEl).toBeInTheDocument();
   });
 
   test('Verifica se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
-    const imageEl = screen.getByRole('img', { name: /Pikachu crying because/i });
+    const imageEl = screen.getByRole('img', { name: /pikachu crying because/i });
     expect(imageEl).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
