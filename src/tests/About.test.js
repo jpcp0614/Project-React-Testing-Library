006c010ba1ca877775fffcc1665b345142d244a0
jpcp0614 <jpcp0614@gmail.com>
2021-11-05 17:39:45 -0300

    Inicia requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import About from '../components/About';
+
+describe('Requisito 2. Testa o componente About.js', () => {
+  test('Verifica se a página contém as informações da Pokédex', () => {
+
+  });
+
+  test('Verifica se a página contém um heading h2 com o texto About Pokédex', () => {
+
+  });
+
+  test('Verifica se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+
+  });
+
+  test('Verifica se a página contém a imagem de uma Pokédex', () => {
+
+  });
+});
